34d5763e4c9870122460d392703d7579ffd44e03
sechiro <sechiro@zbxms01.(none)>
2013-04-07 01:01:45 +0900

    Add detailed metrics to templates

--- make_all_template.sh
+++ make_all_template.sh
@@ -6,6 +6,6 @@ cdh_version=$2
 
 for i in 50030 50060 50070 50075 50090
 do
-    ./get_hadoop_jmx.pl dummy_arg $target_host $i --nosend | ./convert_to_hadoop_tmpl.pl > ./template/template_for_${cdh_version}_port_${i}.xml
+    ./get_hadoop_jmx.pl dummy_arg $target_host $i --detailed --nosend | ./convert_to_hadoop_tmpl.pl > ./template/template_for_${cdh_version}_port_${i}.xml
 done
 
